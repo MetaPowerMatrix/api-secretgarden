28c74f69d411a008c25696c16700977bb6a95b43
MetaPowerMatrix <134223401+MetaPowerMatrix@users.noreply.github.com>
2025-05-04 13:56:31 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ import uvicorn
 # 添加项目根目录到Python路径
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from app.main import app
+# from app.main import app
 from app.config import settings
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "app.main:app",
         host="0.0.0.0",
         port=settings.APP_PORT,
         reload=settings.APP_ENV == "development",

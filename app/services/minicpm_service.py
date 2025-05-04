f2570ee495c0d594b0d9735569925521eb439373
MetaPowerMatrix <134223401+MetaPowerMatrix@users.noreply.github.com>
2025-05-04 19:32:57 +0800

    Update minicpm_service.py

--- app/services/minicpm_service.py
+++ app/services/minicpm_service.py
@@ -45,7 +45,7 @@ def load_model():
                 attn_implementation='sdpa', torch_dtype=torch.bfloat16,
                 init_vision=False, init_audio=True, init_tts=True)
         
-        device_map = infer_auto_device_map(model, max_memory={0: "12GB", 1: "12GB"},
+        device_map = infer_auto_device_map(model, max_memory={0: "10GB", 1: "10GB"},
             no_split_module_classes=['SiglipVisionTransformer', 'Qwen2DecoderLayer'])
         device_id = device_map["llm.model.embed_tokens"]
         device_map["llm.lm_head"] = device_id # first and last layer should be in same device
@@ -62,6 +62,8 @@ def load_model():
         device_map["llm.model.layers.15"] = device_id2
         device_map["llm.model.layers.16"] = device_id2
         # print(device_map)
+
+        model.device = device_id
 
         model_path = "./minicpm"
         model = load_checkpoint_and_dispatch(model, model_path, dtype=torch.bfloat16, device_map=device_map)
